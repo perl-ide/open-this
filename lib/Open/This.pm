02b8efecb5f38d468d561a1c52022098de8e29dd
Olaf Alders <olaf@wundersolutions.com>
2018-08-23 07:59:18 -0400

    increment $VERSION after 0.000011 release

--- lib/Open/This.pm
+++ lib/Open/This.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Open::This;
 
-our $VERSION = '0.000011';
+our $VERSION = '0.000012';
 
 our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw(parse_text to_editor_args);
